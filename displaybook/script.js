4c7d306fc46c1c4a165ef85e81c0d968044e94e5
rykktiadLoans <datokar1705@gmail.com>
2023-06-11 22:45:19 +0200

    loading text thing

--- displaybook/script.js
+++ displaybook/script.js
@@ -1,4 +1,15 @@
 let title = document.querySelector("#title");
 let text = document.querySelector("#text");
-let textItem = fetch("displaybook/collection.json").then((response) => response.json()).then((json) => {title.innerHTML = json.items[0].title});
+//let textItem = fetch("displaybook/collection.json").then((response) => response.json()).then((json) => {title.innerHTML = json.items[0].title});
 console.log("se");
+
+let changeTitle = function(titleObj, textObj, callback){
+    let textItem = fetch("displaybook/collection.json").then((response) => response.json());
+    callback(textObj, textItem.items[0].id);
+}
+
+let changeText = function(textObj, num){
+    fetch("displaybook/text.json").then((response) => response.json()).then((json) => {textObj.innerHTML = json.items[num]});
+}
+
+changeTitle(title, text, changeText);
