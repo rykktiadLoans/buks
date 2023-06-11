88e6728f44e6f8d907c74d67b124b3b5a65a4009
rykktiadLoans <datokar1705@gmail.com>
2023-06-11 19:12:22 +0200

    testing out

--- displaybook/script.js
+++ displaybook/script.js
@@ -1,3 +1,4 @@
 let title = document.querySelector("#title");
 let text = document.querySelector("#text");
 let textItem = fetch("displaybook/collection.json").then((response) => response.json()).then((json) => {title.innerHTML = json.items[0].title});
+console.log("se");
