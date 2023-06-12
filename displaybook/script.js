391766e6691c28827ce3b451a0de1bd78685da00
rykktiadLoans <datokar1705@gmail.com>
2023-06-12 11:51:00 +0200

    made a collection on main page

--- displaybook/script.js
+++ displaybook/script.js
@@ -1,7 +1,5 @@
 let title = document.querySelector("#title");
 let text = document.querySelector("#text");
-//let textItem = fetch("displaybook/collection.json").then((response) => response.json()).then((json) => {title.innerHTML = json.items[0].title});
-console.log("se");
 
 let changeTitle = function(titleObj, textObj, callback){
     let textItem = fetch("displaybook/collection.json").then((response) => response.json()).then(result => {
---
+++ script.js
@@ -0,0 +1,14 @@
+async function getJson(file){
+    let j = await fetch("file").then((response) => response.json());
+    return j;
+}
+
+getJson("displaybook/collection.json").then((json) => {
+    let sub = document.querySelectorAll(".subcollection");
+    sub.forEach(element => {
+        element.innerHTML = "";
+    });
+    for(let i = 0; i < json.items.length; i++){
+        sub[i % 3].innerHTML += json.items[i].title;
+    }
+});
